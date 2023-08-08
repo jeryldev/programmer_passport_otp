544dd5f0a7c89106a34c81a45872a345c78ce2e2
jeryldev <jeryldev@gmail.com>
2023-08-08 10:08:34 +0800

    create calculator tests

--- calculator/test/calculator_test.exs
+++ calculator/test/calculator_test.exs
@@ -1,3 +1,39 @@
 defmodule CalculatorTest do
   use ExUnit.Case
+  alias Calculator
+
+  setup do
+    initial_state = 0
+    server = Calculator.start(initial_state)
+    {:ok, server: server, state: initial_state}
+  end
+
+  test "add number to calculator state", %{server: server, state: state} do
+    assert Calculator.add(server, 1) == :ok
+    assert Calculator.state(server) == state + 1
+  end
+
+  test "subtract number from calculator state", %{server: server, state: state} do
+    assert Calculator.subtract(server, 1) == :ok
+    assert Calculator.state(server) == state - 1
+  end
+
+  test "multiply number with calculator state", %{server: server, state: state} do
+    assert Calculator.multiply(server, 1) == :ok
+    assert Calculator.state(server) == state * 1
+  end
+
+  test "divide calculator state by number", %{server: server, state: state} do
+    assert Calculator.divide(server, 1) == :ok
+    assert Calculator.state(server) == state / 1
+  end
+
+  test "clear calculator state", %{server: server} do
+    assert Calculator.clear(server) == :ok
+    assert Calculator.state(server) == 0
+  end
+
+  test "get calculator state", %{server: server, state: state} do
+    assert Calculator.state(server) == state
+  end
 end
