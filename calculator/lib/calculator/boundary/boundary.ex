d1ef1049e3875266b79cfc65f19a3e56b9351da6
jeryldev <jeryldev@gmail.com>
2023-08-08 09:32:18 +0800

    added notes to boundary

--- calculator/lib/calculator/boundary/boundary.ex
+++ calculator/lib/calculator/boundary/boundary.ex
@@ -1,34 +1,9 @@
 defmodule Calculator.Boundary.Boundary do
   alias Calculator.Core
 
-  def listen(state) do
-    receive do
-      {:add, number} ->
-        Core.add(state, number)
 
-      {:subtract, number} ->
-        Core.subtract(state, number)
-
-      {:multiply, number} ->
-        Core.multiply(state, number)
-
-      {:divide, number} ->
-        Core.divide(state, number)
-
-      {:fold, list, acc, function} ->
-        Core.fold(list, acc, function)
-
-      {:clear} ->
-        0
-
-      {:state, pid} ->
-        send(pid, {:state, state})
-        state
-
-      unexpected ->
-        IO.puts("Unexpected message: #{inspect(unexpected)}")
-        listen(state)
-    end
+  def start(initial_state) do
+    spawn(fn -> run(initial_state) end) # <--- init
   end
 
   def run(state) do
@@ -37,7 +12,33 @@ defmodule Calculator.Boundary.Boundary do
     |> run
   end
 
-  def start(initial_state) do
-    spawn(fn -> run(initial_state) end)
+  def listen(state) do
+    receive do
+      {:add, number} ->
+        Core.add(state, number) # <--- handle_cast
+
+      {:subtract, number} ->
+        Core.subtract(state, number) # <--- handle_cast
+
+      {:multiply, number} ->
+        Core.multiply(state, number) # <--- handle_cast
+
+      {:divide, number} ->
+        Core.divide(state, number) # <--- handle_cast
+
+      {:fold, list, acc, function} ->
+        Core.fold(list, acc, function) # <--- handle_cast
+
+      {:clear} ->
+        0 # <--- handle_cast
+
+      {:state, pid} ->
+        send(pid, {:state, state}) # <--- handle_call
+        state
+
+      unexpected ->
+        IO.puts("Unexpected message: #{inspect(unexpected)}")
+        listen(state)
+    end
   end
 end
