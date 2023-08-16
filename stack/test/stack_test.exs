964e2d30b4084666a555fb7d80f45ab9c7341479
jeryldev <jeryldev@gmail.com>
2023-08-16 12:32:55 +0800

    create stack test

--- stack/test/stack_test.exs
+++ stack/test/stack_test.exs
@@ -1,8 +1,29 @@
 defmodule StackTest do
   use ExUnit.Case
-  doctest Stack
+  alias Stack
 
-  test "greets the world" do
-    assert Stack.hello() == :world
+  setup do
+    initial_state = "apple"
+    server = Stack.start(initial_state)
+
+    {:ok, server: server, state: initial_state}
+  end
+
+  test "push", %{server: server, state: state} do
+    Stack.push(server, "banana")
+    assert Stack.state(server) == ["banana", "apple"]
+  end
+
+  test "pop", %{server: server, state: state} do
+    Stack.push(server, "banana")
+    Stack.push(server, "cherry")
+    Stack.push(server, "date")
+    assert Stack.state(server) == ["date", "cherry", "banana", "apple"]
+    assert Stack.pop(server) == "date"
+    assert Stack.state(server) == ["cherry", "banana", "apple"]
+  end
+
+  test "state", %{server: server, state: state} do
+    assert Stack.state(server) == ["apple"]
   end
 end
